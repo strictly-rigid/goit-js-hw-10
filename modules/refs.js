ea8218f60ad520df1ae5426025409af8ae023ad6
Denys Lavryk <121813691+strictly-rigid@users.noreply.github.com>
2023-05-29 22:04:38 +0300

    refs export fixed

--- modules/refs.js
+++ modules/refs.js
@@ -1,4 +1,4 @@
-export default refs = {
+export const refs = {
     selector: document.querySelector('.breed-select'),
     loadingMsg: document.querySelector('.loader'),
     errorMsg: document.querySelector('.error'),
