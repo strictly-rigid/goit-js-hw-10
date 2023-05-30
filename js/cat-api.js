0f682df283ac8e0c4326ce37e7d427e447bb5b01
Denys Lavryk <121813691+strictly-rigid@users.noreply.github.com>
2023-05-30 19:30:11 +0300

    fixed api functions return only promise

--- js/cat-api.js
+++ js/cat-api.js
@@ -6,7 +6,7 @@ const API_KEY = 'live_ag5gUMuzkKy1xkRlgH6wDth7ng0wrLjfzgajka7bj4vCf5eioDJ3pQ1w34
 
 export function fetchBreeds() {
     
-fetch("https://api.thecatapi.com/v1/breeds")
+return fetch("https://api.thecatapi.com/v1/breeds")
     .then(response => {
         if (!response.ok) {
         throw new Error(response.status);
@@ -14,31 +14,13 @@ fetch("https://api.thecatapi.com/v1/breeds")
        
         return response.json();
     })
-    .then(data => {     
-        hideLoadingMsg(); 
-      
-        refs.selector.classList.remove('visually-hidden');
-        refs.errorMsg.classList.add('visually-hidden');
-        
-        data.forEach(breed => {
-        const catBreed = document.createElement('option');
-        catBreed.value = breed.id;
-        catBreed.textContent = breed.name;
-        refs.selector.appendChild(catBreed);
-        
-    });
-    })
-    .catch(error => { 
-        hideLoadingMsg();
-        refs.errorMsg.classList.remove("visually-hidden");
-        console.error('Error:', error.name, error.message);
-    });
+    
 }
 
 
 export function fetchCatByBreed(breedId) {
     showLoadingMsg();
-    fetch(`https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}&breed_ids=${breedId}`).then(
+    return fetch(`https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}&breed_ids=${breedId}`).then(
         response => {
             
             if (!response.ok) {
@@ -48,24 +30,10 @@ export function fetchCatByBreed(breedId) {
         })
         .then(data => {
                 if (data && data.length > 0) {
-            hideLoadingMsg();
-            const breedData = data[0];
-            console.log(breedData);
-            let breedImg = document.createElement('img');
-                    breedImg.src = breedData.url;
-                    breedImg.style.maxWidth = "320px"
-            let breedName = document.createElement('h3');
-                    breedName.textContent = breedData.breeds[0].name;
-            let breedDescr = document.createElement('p');
-                    breedDescr.textContent = breedData.description;
-            let breedTemper = document.createElement('p');
-                    breedTemper.textContent = breedData.breeds[0].temperament;
-            refs.infoArea.innerHTML = '';
-            refs.infoArea.append(breedImg, breedName, breedDescr, breedTemper);}
-       
-            
-
-            
+                    hideLoadingMsg();
+                    return data[0];
+                          }
+                             
         })
         .catch(error => {
             hideLoadingMsg();
--- js/index.js
+++ js/index.js
@@ -7,14 +7,50 @@ hideLoadingMsg();
 refs.selector.classList.add('visually-hidden');
 refs.errorMsg.classList.add("visually-hidden");
 
-fetchBreeds();
+fetchBreeds()
+    .then(data => {     
+        hideLoadingMsg(); 
+      
+        refs.selector.classList.remove('visually-hidden');
+        refs.errorMsg.classList.add('visually-hidden');
+        
+        data.forEach(breed => {
+        const catBreed = document.createElement('option');
+        catBreed.value = breed.id;
+        catBreed.textContent = breed.name;
+        refs.selector.appendChild(catBreed);
+        
+    });
+    })
+    .catch(error => { 
+        hideLoadingMsg();
+        refs.errorMsg.classList.remove("visually-hidden");
+        console.error('Error:', error.name, error.message);
+    });
 
 refs.selector.addEventListener('change', () => {
     const breedId = refs.selector.value;
     fetchCatByBreed(breedId)
+        .then(breedData => {
+            if (breedData) {
+                console.log(breedData);
+            let breedImg = document.createElement('img');
+                    breedImg.src = breedData.url;
+                    breedImg.style.maxWidth = "320px"
+            let breedName = document.createElement('h3');
+                    breedName.textContent = breedData.breeds[0].name;
+            let breedDescr = document.createElement('p');
+                    breedDescr.textContent = breedData.breeds[0].description;
+            let breedTemper = document.createElement('p');
+                    breedTemper.innerHTML = '<b>Temperament: </b>' + breedData.breeds[0].temperament;
+            refs.infoArea.innerHTML = '';
+                    refs.infoArea.append(breedImg, breedName, breedDescr, breedTemper);
+             }
+        })
+    
 });
 
-fetchCatByBreed();
+
 
 
 
